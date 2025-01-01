39630d367ff25498baf8e6e2c39ee8785f57c75b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-01 11:56:01 +0100

    Deploy website - based on e62b352e2075460b83493ec73659fdb93fcaebbf

---
+++ assets/js/acecf23e.26644830.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_inversifyjs_inversify_docs_site=self.webpackChunk_inversifyjs_inversify_docs_site||[]).push([[903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/monorepo/blog","blogTitle":"Blog","authorsListPath":"/monorepo/blog/authors"}')}}]);
