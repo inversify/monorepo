0d8b55097d445bf6d2de5344114f9acb1b85e4f8
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-03-16 11:31:13 +0100

    fix(@inversifyjs/http-core): sort RequestMethodType values

--- packages/http/libraries/core/src/http/models/RequestMethodType.ts
+++ packages/http/libraries/core/src/http/models/RequestMethodType.ts
@@ -1,10 +1,10 @@
 export enum RequestMethodType {
+  ALL = 'all',
+  DELETE = 'delete',
   GET = 'get',
+  HEAD = 'head',
+  OPTIONS = 'options',
+  PATCH = 'patch',
   POST = 'post',
   PUT = 'put',
-  DELETE = 'delete',
-  PATCH = 'patch',
-  OPTIONS = 'options',
-  HEAD = 'head',
-  ALL = 'all',
 }
