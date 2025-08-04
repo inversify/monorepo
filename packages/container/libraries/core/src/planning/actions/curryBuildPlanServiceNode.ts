14123eb6df695e09cce6bffff054ac9ce991f675
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-08-04 02:51:39 +0200

    refactor(core): add curryBuildPlanServiceNode

---
+++ packages/container/libraries/core/src/planning/actions/curryBuildPlanServiceNode.ts
@@ -0,0 +1,77 @@
+import { Binding } from '../../binding/models/Binding';
+import { BindingConstraints } from '../../binding/models/BindingConstraints';
+import {
+  BindingConstraintsImplementation,
+  InternalBindingConstraints,
+} from '../../binding/models/BindingConstraintsImplementation';
+import { SingleInmutableLinkedList } from '../../common/models/SingleInmutableLinkedList';
+import { buildFilteredServiceBindings } from '../calculations/buildFilteredServiceBindings';
+import { buildPlanBindingConstraintsList } from '../calculations/buildPlanBindingConstraintsList';
+import { checkServiceNodeSingleInjectionBindings } from '../calculations/checkServiceNodeSingleInjectionBindings';
+import { BasePlanParams } from '../models/BasePlanParams';
+import { BindingNodeParent } from '../models/BindingNodeParent';
+import { PlanBindingNode } from '../models/PlanBindingNode';
+import { PlanParams } from '../models/PlanParams';
+import { PlanServiceNode } from '../models/PlanServiceNode';
+
+export function curryBuildPlanServiceNode(
+  buildServiceNodeBindings: (
+    params: BasePlanParams,
+    bindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints>,
+    serviceBindings: Binding<unknown>[],
+    parentNode: BindingNodeParent,
+    chainedBindings: boolean,
+  ) => PlanBindingNode[],
+): (params: PlanParams) => PlanServiceNode {
+  return (params: PlanParams): PlanServiceNode => {
+    const bindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints> =
+      buildPlanBindingConstraintsList(params);
+
+    const bindingConstraints: BindingConstraints =
+      new BindingConstraintsImplementation(bindingConstraintsList.last);
+
+    const chained: boolean = params.rootConstraints.isMultiple
+      ? params.rootConstraints.chained
+      : false;
+
+    const filteredServiceBindings: Binding<unknown>[] =
+      buildFilteredServiceBindings(params, bindingConstraints, {
+        chained,
+      });
+
+    const serviceNodeBindings: PlanBindingNode[] = [];
+
+    const serviceNode: PlanServiceNode = {
+      bindings: serviceNodeBindings,
+      isContextFree: true,
+      serviceIdentifier: params.rootConstraints.serviceIdentifier,
+    };
+
+    serviceNodeBindings.push(
+      ...buildServiceNodeBindings(
+        params,
+        bindingConstraintsList,
+        filteredServiceBindings,
+        serviceNode,
+        chained,
+      ),
+    );
+
+    serviceNode.isContextFree =
+      !bindingConstraintsList.last.elem.getAncestorsCalled;
+
+    if (!params.rootConstraints.isMultiple) {
+      checkServiceNodeSingleInjectionBindings(
+        serviceNode,
+        params.rootConstraints.isOptional ?? false,
+        bindingConstraintsList.last,
+      );
+
+      const [planBindingNode]: PlanBindingNode[] = serviceNodeBindings;
+
+      serviceNode.bindings = planBindingNode;
+    }
+
+    return serviceNode;
+  };
+}
