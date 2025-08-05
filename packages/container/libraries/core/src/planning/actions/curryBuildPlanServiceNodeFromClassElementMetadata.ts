ad688766631795408b209c4fbff451101f0e4ba1
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-08-05 21:00:37 +0200

    refactor: extract curryBuildPlanServiceNodeFromClassElementMetadata

---
+++ packages/container/libraries/core/src/planning/actions/curryBuildPlanServiceNodeFromClassElementMetadata.ts
@@ -0,0 +1,97 @@
+import { ServiceIdentifier } from '@inversifyjs/common';
+
+import { Binding } from '../../binding/models/Binding';
+import { BindingConstraints } from '../../binding/models/BindingConstraints';
+import {
+  BindingConstraintsImplementation,
+  InternalBindingConstraints,
+} from '../../binding/models/BindingConstraintsImplementation';
+import { SingleInmutableLinkedList } from '../../common/models/SingleInmutableLinkedList';
+import { ClassElementMetadataKind } from '../../metadata/models/ClassElementMetadataKind';
+import { ManagedClassElementMetadata } from '../../metadata/models/ManagedClassElementMetadata';
+import { buildFilteredServiceBindings } from '../calculations/buildFilteredServiceBindings';
+import { checkServiceNodeSingleInjectionBindings } from '../calculations/checkServiceNodeSingleInjectionBindings';
+import { getServiceFromMaybeLazyServiceIdentifier } from '../calculations/getServiceFromMaybeLazyServiceIdentifier';
+import { BasePlanParams } from '../models/BasePlanParams';
+import { BindingNodeParent } from '../models/BindingNodeParent';
+import { PlanBindingNode } from '../models/PlanBindingNode';
+import { PlanServiceNode } from '../models/PlanServiceNode';
+import { SubplanParams } from '../models/SubplanParams';
+
+export function curryBuildPlanServiceNodeFromClassElementMetadata(
+  buildServiceNodeBindings: (
+    params: BasePlanParams,
+    bindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints>,
+    serviceBindings: Binding<unknown>[],
+    parentNode: BindingNodeParent,
+    chainedBindings: boolean,
+  ) => PlanBindingNode[],
+): (
+  params: SubplanParams,
+  bindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints>,
+  elementMetadata: ManagedClassElementMetadata,
+) => PlanServiceNode {
+  return (
+    params: SubplanParams,
+    bindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints>,
+    elementMetadata: ManagedClassElementMetadata,
+  ): PlanServiceNode => {
+    const serviceIdentifier: ServiceIdentifier =
+      getServiceFromMaybeLazyServiceIdentifier(elementMetadata.value);
+
+    const updatedBindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints> =
+      bindingConstraintsList.concat({
+        getAncestorsCalled: false,
+        name: elementMetadata.name,
+        serviceIdentifier,
+        tags: elementMetadata.tags,
+      });
+
+    const bindingConstraints: BindingConstraints =
+      new BindingConstraintsImplementation(updatedBindingConstraintsList.last);
+
+    const chained: boolean =
+      elementMetadata.kind === ClassElementMetadataKind.multipleInjection &&
+      elementMetadata.chained;
+
+    const filteredServiceBindings: Binding<unknown>[] =
+      buildFilteredServiceBindings(params, bindingConstraints, {
+        chained,
+      });
+
+    const serviceNodeBindings: PlanBindingNode[] = [];
+
+    const serviceNode: PlanServiceNode = {
+      bindings: serviceNodeBindings,
+      isContextFree: true,
+      serviceIdentifier,
+    };
+
+    serviceNodeBindings.push(
+      ...buildServiceNodeBindings(
+        params,
+        updatedBindingConstraintsList,
+        filteredServiceBindings,
+        serviceNode,
+        chained,
+      ),
+    );
+
+    serviceNode.isContextFree =
+      !updatedBindingConstraintsList.last.elem.getAncestorsCalled;
+
+    if (elementMetadata.kind === ClassElementMetadataKind.singleInjection) {
+      checkServiceNodeSingleInjectionBindings(
+        serviceNode,
+        elementMetadata.optional,
+        updatedBindingConstraintsList.last,
+      );
+
+      const [planBindingNode]: PlanBindingNode[] = serviceNodeBindings;
+
+      serviceNode.bindings = planBindingNode;
+    }
+
+    return serviceNode;
+  };
+}
