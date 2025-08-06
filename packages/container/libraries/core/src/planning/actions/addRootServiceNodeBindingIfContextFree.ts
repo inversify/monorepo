58b7e14b52ec10439c2419475be7f179a70f6a96
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-08-07 00:23:54 +0200

    refactor(core): add addRootServiceNodeBindingIfContextFree

---
+++ packages/container/libraries/core/src/planning/actions/addRootServiceNodeBindingIfContextFree.ts
@@ -0,0 +1,43 @@
+import { Binding } from '../../binding/models/Binding';
+import { InternalBindingConstraints } from '../../binding/models/BindingConstraintsImplementation';
+import { SingleInmutableLinkedList } from '../../common/models/SingleInmutableLinkedList';
+import { PlanServiceNodeBindingAddedResult } from '../../metadata/models/PlanServiceNodeUpdateResult';
+import { buildPlanBindingConstraintsList } from '../calculations/buildPlanBindingConstraintsList';
+import { LazyPlanServiceNode } from '../models/LazyPlanServiceNode';
+import { PlanParams } from '../models/PlanParams';
+import { PlanServiceNode } from '../models/PlanServiceNode';
+import { addServiceNodeBindingIfContextFree } from './addServiceNodeBindingIfContextFree';
+
+/**
+ * Attach a binding to the root service node if the binding is context-free.
+ * @param params The plan parameters.
+ * @param serviceNode The service node to attach the binding to.
+ * @param binding The binding to attach.
+ * @returns True if the binding requires ancestor metadata, false otherwise.
+ */
+export function addRootServiceNodeBindingIfContextFree(
+  params: PlanParams,
+  serviceNode: PlanServiceNode,
+  binding: Binding<unknown>,
+): PlanServiceNodeBindingAddedResult {
+  if (LazyPlanServiceNode.is(serviceNode) && !serviceNode.isExpanded()) {
+    return {
+      isContextFreeBinding: true,
+      shouldInvalidateServiceNode: false,
+    };
+  }
+
+  const bindingConstraintsList: SingleInmutableLinkedList<InternalBindingConstraints> =
+    buildPlanBindingConstraintsList(params);
+
+  const chained: boolean =
+    params.rootConstraints.isMultiple && params.rootConstraints.chained;
+
+  return addServiceNodeBindingIfContextFree(
+    params,
+    serviceNode,
+    binding,
+    bindingConstraintsList,
+    chained,
+  );
+}
