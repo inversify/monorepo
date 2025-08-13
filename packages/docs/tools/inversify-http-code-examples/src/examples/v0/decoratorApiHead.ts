c1fcdd7a0a9cc9259ae07bf91d4a168bd1602324
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-08-13 12:48:44 +0200

    fix: update InversifyFastifyHttpAdapter._buildRouter
    update method to build normalized urls

--- packages/docs/tools/inversify-http-code-examples/src/examples/v0/decoratorApiHead.ts
+++ packages/docs/tools/inversify-http-code-examples/src/examples/v0/decoratorApiHead.ts
@@ -1,16 +1,9 @@
-import {
-  Controller,
-  Head,
-  HttpStatusCode,
-  SetHeader,
-  StatusCode,
-} from '@inversifyjs/http-core';
+import { Controller, Head, SetHeader } from '@inversifyjs/http-core';
 
 @Controller('/content')
 export class ContentController {
   @Head()
   @SetHeader('custom-content-header', 'sample')
-  @StatusCode(HttpStatusCode.OK)
   public async headContent(): Promise<undefined> {
     return undefined;
   }
