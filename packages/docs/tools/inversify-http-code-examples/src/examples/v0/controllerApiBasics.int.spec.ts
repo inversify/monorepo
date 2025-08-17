25d80080adf4d6eea6ecf38b0fd9cc72321ce428
notaphplover <roberto.pintos.lopez@gmail.com>
2025-08-17 16:56:05 +0200

    feat: add controller code examples

---
+++ packages/docs/tools/inversify-http-code-examples/src/examples/v0/controllerApiBasics.int.spec.ts
@@ -0,0 +1,42 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import { Container } from 'inversify';
+
+import { buildExpressServer } from '../../server/adapter/express/actions/buildExpressServer';
+import { buildExpress4Server } from '../../server/adapter/express4/actions/buildExpress4Server';
+import { buildFastifyServer } from '../../server/adapter/fastify/actions/buildFastifyServer';
+import { buildHonoServer } from '../../server/adapter/hono/actions/buildHonoServer';
+import { Server } from '../../server/models/Server';
+import { Message, MessagesController } from './controllerApiBasics';
+
+describe.each<[(container: Container) => Promise<Server>]>([
+  [buildExpress4Server],
+  [buildExpressServer],
+  [buildFastifyServer],
+  [buildHonoServer],
+])(
+  'Controller (basics)',
+  (buildServer: (container: Container) => Promise<Server>) => {
+    let server: Server;
+
+    beforeAll(async () => {
+      const container: Container = new Container();
+      container.bind(MessagesController).toSelf().inSingletonScope();
+
+      server = await buildServer(container);
+    });
+
+    afterAll(async () => {
+      await server.shutdown();
+    });
+
+    it('should handle GET /messages/hello', async () => {
+      const response: Response = await fetch(
+        `http://${server.host}:${server.port.toString()}/messages/hello`,
+      );
+      const body: Message = (await response.json()) as Message;
+
+      expect(body).toStrictEqual({ content: 'world' });
+    });
+  },
+);
