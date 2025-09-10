af7466656ccaf46a06cd8192206750dd8b4f9ff4
notaphplover <roberto.pintos.lopez@gmail.com>
2025-09-10 12:41:17 +0200

    style: fix linter issues

--- packages/container/libraries/core/src/metadata/calculations/buildMaybeClassElementMetadataFromMaybeClassElementMetadata.spec.ts
+++ packages/container/libraries/core/src/metadata/calculations/buildMaybeClassElementMetadataFromMaybeClassElementMetadata.spec.ts
@@ -8,91 +8,88 @@ import { MaybeManagedClassElementMetadata } from '../models/MaybeManagedClassEle
 import { UnmanagedClassElementMetadata } from '../models/UnmanagedClassElementMetadata';
 import { buildMaybeClassElementMetadataFromMaybeClassElementMetadata } from './buildMaybeClassElementMetadataFromMaybeClassElementMetadata';
 
-describe(
-  buildMaybeClassElementMetadataFromMaybeClassElementMetadata.name,
-  () => {
-    describe('having unmanaged metadata', () => {
-      let metadataFixture: UnmanagedClassElementMetadata;
-      let updateMetadataMock: Mock<
-        (
-          metadata:
-            | ManagedClassElementMetadata
-            | MaybeManagedClassElementMetadata,
-        ) => ManagedClassElementMetadata | MaybeManagedClassElementMetadata
-      >;
+describe(buildMaybeClassElementMetadataFromMaybeClassElementMetadata, () => {
+  describe('having unmanaged metadata', () => {
+    let metadataFixture: UnmanagedClassElementMetadata;
+    let updateMetadataMock: Mock<
+      (
+        metadata:
+          | ManagedClassElementMetadata
+          | MaybeManagedClassElementMetadata,
+      ) => ManagedClassElementMetadata | MaybeManagedClassElementMetadata
+    >;
 
-      beforeAll(() => {
-        metadataFixture = {
-          kind: ClassElementMetadataKind.unmanaged,
-        };
-        updateMetadataMock = vitest.fn();
-      });
-
-      describe('when called', () => {
-        let result: unknown;
-
-        beforeAll(() => {
-          try {
-            buildMaybeClassElementMetadataFromMaybeClassElementMetadata(
-              updateMetadataMock,
-            )(metadataFixture);
-          } catch (error: unknown) {
-            result = error;
-          }
-        });
-
-        it('should throw an InversifyCoreError', () => {
-          const expectedErrorProperties: Partial<InversifyCoreError> = {
-            kind: InversifyCoreErrorKind.injectionDecoratorConflict,
-            message:
-              'Unexpected injection found. Found @unmanaged injection with additional @named, @optional, @tagged or @targetName injections',
-          };
-
-          expect(result).toBeInstanceOf(InversifyCoreError);
-          expect(result).toStrictEqual(
-            expect.objectContaining(expectedErrorProperties),
-          );
-        });
-      });
+    beforeAll(() => {
+      metadataFixture = {
+        kind: ClassElementMetadataKind.unmanaged,
+      };
+      updateMetadataMock = vitest.fn();
     });
 
-    describe('having non unmanaged metadata', () => {
-      let metadataFixture: ManagedClassElementMetadata;
-      let updateMetadataMock: Mock<
-        (
-          metadata:
-            | ManagedClassElementMetadata
-            | MaybeManagedClassElementMetadata,
-        ) => ManagedClassElementMetadata | MaybeManagedClassElementMetadata
-      >;
+    describe('when called', () => {
+      let result: unknown;
 
       beforeAll(() => {
-        metadataFixture = {
-          kind: ClassElementMetadataKind.singleInjection,
-          name: undefined,
-          optional: false,
-          tags: new Map([['foo', 'bar']]),
-          value: 'service-identifier',
-        };
-        updateMetadataMock = vitest.fn();
+        try {
+          buildMaybeClassElementMetadataFromMaybeClassElementMetadata(
+            updateMetadataMock,
+          )(metadataFixture);
+        } catch (error: unknown) {
+          result = error;
+        }
       });
 
-      describe('when called', () => {
-        let result: unknown;
+      it('should throw an InversifyCoreError', () => {
+        const expectedErrorProperties: Partial<InversifyCoreError> = {
+          kind: InversifyCoreErrorKind.injectionDecoratorConflict,
+          message:
+            'Unexpected injection found. Found @unmanaged injection with additional @named, @optional, @tagged or @targetName injections',
+        };
 
-        beforeAll(() => {
-          updateMetadataMock.mockReturnValueOnce(metadataFixture);
-
-          result =
-            buildMaybeClassElementMetadataFromMaybeClassElementMetadata(
-              updateMetadataMock,
-            )(metadataFixture);
-        });
-
-        it('should return ManagedClassElementMetadata', () => {
-          expect(result).toBe(metadataFixture);
-        });
+        expect(result).toBeInstanceOf(InversifyCoreError);
+        expect(result).toStrictEqual(
+          expect.objectContaining(expectedErrorProperties),
+        );
       });
     });
-  },
-);
+  });
+
+  describe('having non unmanaged metadata', () => {
+    let metadataFixture: ManagedClassElementMetadata;
+    let updateMetadataMock: Mock<
+      (
+        metadata:
+          | ManagedClassElementMetadata
+          | MaybeManagedClassElementMetadata,
+      ) => ManagedClassElementMetadata | MaybeManagedClassElementMetadata
+    >;
+
+    beforeAll(() => {
+      metadataFixture = {
+        kind: ClassElementMetadataKind.singleInjection,
+        name: undefined,
+        optional: false,
+        tags: new Map([['foo', 'bar']]),
+        value: 'service-identifier',
+      };
+      updateMetadataMock = vitest.fn();
+    });
+
+    describe('when called', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        updateMetadataMock.mockReturnValueOnce(metadataFixture);
+
+        result =
+          buildMaybeClassElementMetadataFromMaybeClassElementMetadata(
+            updateMetadataMock,
+          )(metadataFixture);
+      });
+
+      it('should return ManagedClassElementMetadata', () => {
+        expect(result).toBe(metadataFixture);
+      });
+    });
+  });
+});
