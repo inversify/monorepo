527954d50ab03b4fdfe525b08e42671ec9549b56
notaphplover <roberto.pintos.lopez@gmail.com>
2025-09-16 13:21:09 +0200

    feat: expose ajv modules

---
+++ packages/framework/validation/libraries/ajv/src/index.ts
@@ -0,0 +1,3 @@
+export { ValidateAjvSchema } from './decorators/ValidateAjvSchema';
+export { AjvCompiledValidationPipe } from './pipes/AjvCompiledValidationPipe';
+export { AjvValidationPipe } from './pipes/AjvValidationPipe';
