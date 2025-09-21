c8ae5407d41453e27dffcc2649171674ebb49f0d
notaphplover <roberto.pintos.lopez@gmail.com>
2025-09-21 21:40:44 +0200

    refactor: add getBetterAuthFromRequest

---
+++ packages/framework/http/libraries/better-auth/src/auth/calculations/getBetterAuthFromRequest.ts
@@ -0,0 +1,21 @@
+import { InternalServerErrorHttpResponse } from '@inversifyjs/http-core';
+import { BetterAuthOptions } from 'better-auth';
+
+import { BetterAuth } from '../models/BetterAuth';
+import { betterAuthProperty } from '../models/betterAuthProperty';
+
+export function getBetterAuthFromRequest<TOptions extends BetterAuthOptions>(
+  request: object,
+): BetterAuth<TOptions> {
+  const auth: BetterAuth<TOptions> | undefined = (
+    request as unknown as Record<string | symbol, unknown>
+  )[betterAuthProperty] as BetterAuth<TOptions> | undefined;
+
+  if (auth === undefined) {
+    throw new InternalServerErrorHttpResponse(
+      'BetterAuth auth not found when accessing user session. Did you forget to apply the BetterAuth middleware?',
+    );
+  }
+
+  return auth;
+}
