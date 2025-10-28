21b6e367da00d630ba8ceb21d995b376f6c24c90
notaphplover <roberto.pintos.lopez@gmail.com>
2025-10-28 02:09:01 +0100

    style: fix typo

--- packages/framework/http/libraries/express-v4/src/index.ts
+++ packages/framework/http/libraries/express-v4/src/index.ts
@@ -1,6 +1,6 @@
 export { InversifyExpressHttpAdapter } from './adapter/InversifyExpressHttpAdapter';
 
 export type { ExpressErrorFilter } from './models/ExpressErrorFilter';
-export type { ExpressGuard } from './models/ExpresssGuard';
+export type { ExpressGuard } from './models/ExpressGuard';
 export type { ExpressInterceptor } from './models/ExpressInterceptor';
 export type { ExpressMiddleware } from './models/ExpressMiddleware';
