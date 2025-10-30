360f6f94ba3f7a8337fc56e85f0975b549c63eec
notaphplover <roberto.pintos.lopez@gmail.com>
2025-10-30 16:44:54 +0100

    fix: fix wrong stream imports

--- packages/framework/http/libraries/core/src/httpResponse/models/MethodNotAllowedHttpResponse.ts
+++ packages/framework/http/libraries/core/src/httpResponse/models/MethodNotAllowedHttpResponse.ts
@@ -1,4 +1,4 @@
-import Stream from 'node:stream';
+import type { Stream } from 'node:stream';
 
 import { HttpStatusCode } from '../../http/models/HttpStatusCode';
 import { ErrorHttpResponse } from './ErrorHttpResponse';
