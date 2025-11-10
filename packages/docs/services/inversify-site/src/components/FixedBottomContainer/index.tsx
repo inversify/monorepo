ce5f3a2e40bacda37adc12b9dccad962a699bd14
notaphplover <roberto.pintos.lopez@gmail.com>
2025-11-10 10:34:21 +0100

    feat: add FixedBottomContainer component

---
+++ packages/docs/services/inversify-site/src/components/FixedBottomContainer/index.tsx
@@ -0,0 +1,36 @@
+import { Icon } from '@iconify/react';
+import React, { useState } from 'react';
+
+import styles from './styles.module.css';
+
+export interface FixedBottomContainerProps {
+  children: React.ReactNode;
+}
+
+export default function FixedBottomContainer({
+  children,
+}: FixedBottomContainerProps): React.JSX.Element {
+  const [isVisible, setIsVisible]: [
+    boolean,
+    React.Dispatch<React.SetStateAction<boolean>>,
+  ] = useState<boolean>(true);
+
+  if (!isVisible) {
+    return <></>;
+  }
+
+  return (
+    <div className={styles.fixedBottomContainer}>
+      <button
+        className={styles.closeButton}
+        onClick={() => {
+          setIsVisible(false);
+        }}
+        aria-label="Close"
+      >
+        <Icon icon="carbon:close" />
+      </button>
+      {children}
+    </div>
+  );
+}
