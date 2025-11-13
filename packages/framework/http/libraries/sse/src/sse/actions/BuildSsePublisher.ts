28983969f802d278ff06773bf24e01ee978ecb9a
notaphplover <roberto.pintos.lopez@gmail.com>
2025-11-13 13:08:46 +0100

    refactor: wip

---
+++ packages/framework/http/libraries/sse/src/sse/actions/BuildSsePublisher.ts
@@ -0,0 +1,6 @@
+// import { createCustomParameterDecorator } from '@inversifyjs/http-core';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+// export function BuildSsePublisher(): ParameterDecorator {
+//   return createCustomParameterDecorator();
+// }
