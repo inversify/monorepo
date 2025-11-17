cb51288bd0dab53313954ef6d65b57807a83463e
notaphplover <roberto.pintos.lopez@gmail.com>
2025-11-17 16:03:33 +0100

    feat: update getControllerMetadataList
    update function to sort controllers by priority

--- packages/framework/http/libraries/core/src/routerExplorer/calculations/getControllerMetadataList.ts
+++ packages/framework/http/libraries/core/src/routerExplorer/calculations/getControllerMetadataList.ts
@@ -4,5 +4,15 @@ import { controllerMetadataReflectKey } from '../../reflectMetadata/data/control
 import { ControllerMetadata } from '../model/ControllerMetadata';
 
 export function getControllerMetadataList(): ControllerMetadata[] | undefined {
-  return getOwnReflectMetadata(Reflect, controllerMetadataReflectKey);
+  const controllerMetadataList: ControllerMetadata[] | undefined =
+    getOwnReflectMetadata(Reflect, controllerMetadataReflectKey);
+
+  if (controllerMetadataList === undefined) {
+    return undefined;
+  }
+
+  return controllerMetadataList.sort(
+    (first: ControllerMetadata, second: ControllerMetadata): number =>
+      second.priority - first.priority,
+  );
 }
