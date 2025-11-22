387e420bb25f3e3bf4bf67b92524b7958a1f7b54
notaphplover <roberto.pintos.lopez@gmail.com>
2025-11-22 12:01:05 +0100

    fix: add missing type

--- packages/framework/http/libraries/better-auth/src/auth/models/UserSession.ts
+++ packages/framework/http/libraries/better-auth/src/auth/models/UserSession.ts
@@ -1,9 +1,19 @@
-import {
-  BetterAuthOptions,
-  InferSession,
-  InferUser,
-  PrettifyDeep,
-} from 'better-auth';
+import { BetterAuthOptions, InferSession, InferUser } from 'better-auth';
+
+// Utility type to deeply prettify an object type. If used to be exposed in the better-auth package.
+type PrettifyDeep<T> = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [K in keyof T]: T[K] extends (...args: any[]) => any
+    ? T[K]
+    : T[K] extends object
+      ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        T[K] extends any[]
+        ? T[K]
+        : T[K] extends Date
+          ? T[K]
+          : PrettifyDeep<T[K]>
+      : T[K];
+} & {};
 
 export interface UserSession<TOptions extends BetterAuthOptions> {
   session: PrettifyDeep<InferSession<TOptions>>;
