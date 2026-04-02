1044cb3d82e6b6c51d826d8954c85fa00eded2a3
notaphplover <roberto.pintos.lopez@gmail.com>
2026-04-02 19:22:58 +0200

    fix: update FilteredByValueType to handle 3.2 keys properly

--- packages/framework/http/libraries/open-api/src/common/models/FilteredByValueType.ts
+++ packages/framework/http/libraries/open-api/src/common/models/FilteredByValueType.ts
@@ -1,5 +1,9 @@
 export type FilteredByValueType<TObject, TType> = {
-  [TKey in keyof TObject as TObject[TKey] extends TType | undefined
-    ? TKey
-    : never]: TObject[TKey] extends TType | undefined ? TObject[TKey] : never;
+  [TKey in keyof TObject as NonNullable<TObject[TKey]> extends TType
+    ? string extends keyof NonNullable<TObject[TKey]>
+      ? string extends keyof TType
+        ? TKey
+        : never
+      : TKey
+    : never]: TObject[TKey];
 };
