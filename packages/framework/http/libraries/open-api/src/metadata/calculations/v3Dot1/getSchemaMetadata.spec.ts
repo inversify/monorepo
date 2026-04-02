e14e4d880257a57303a72986a2da07c10ad7e3db
notaphplover <roberto.pintos.lopez@gmail.com>
2026-04-02 22:35:35 +0200

    test: refactor test

--- packages/framework/http/libraries/open-api/src/metadata/calculations/v3Dot1/getSchemaMetadata.spec.ts
+++ packages/framework/http/libraries/open-api/src/metadata/calculations/v3Dot1/getSchemaMetadata.spec.ts
@@ -11,93 +11,97 @@ import { type OpenApiSchemaMetadata } from '../../models/v3Dot1/OpenApiSchemaMet
 import { buildDefaultSchemaMetadata } from './buildDefaultSchemaMetadata.js';
 import { getSchemaMetadata } from './getSchemaMetadata.js';
 
+class OpenApiSchemaMetadataFixtures {
+  public static get any(): OpenApiSchemaMetadata {
+    return {
+      customAttributes: undefined,
+      name: 'TestSchema',
+      properties: new Map(),
+      references: new Set(),
+      schema: undefined,
+    };
+  }
+}
+
 describe(getSchemaMetadata, () => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-  let typeFixture: Function;
+  describe('.getSchemaMetadata', () => {
+    describe('having a type', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+      let typeFixture: Function;
 
-  beforeAll(() => {
-    typeFixture = class {};
-  });
+      beforeAll(() => {
+        typeFixture = class {};
+      });
 
-  describe('when called, and getOwnReflectMetadata() returns undefined', () => {
-    let schemaMetadataFixture: OpenApiSchemaMetadata;
+      describe('when called, and getOwnReflectMetadata() returns undefined', () => {
+        let schemaMetadataFixture: OpenApiSchemaMetadata;
 
-    let result: unknown;
+        let result: unknown;
 
-    beforeAll(() => {
-      schemaMetadataFixture = {
-        customAttributes: undefined,
-        name: 'TestSchema',
-        properties: new Map(),
-        references: new Set(),
-        schema: undefined,
-      };
+        beforeAll(() => {
+          schemaMetadataFixture = OpenApiSchemaMetadataFixtures.any;
 
-      vitest
-        .mocked(buildDefaultSchemaMetadata)
-        .mockReturnValueOnce(schemaMetadataFixture);
+          vitest
+            .mocked(buildDefaultSchemaMetadata)
+            .mockReturnValueOnce(schemaMetadataFixture);
 
-      result = getSchemaMetadata(typeFixture);
-    });
+          result = getSchemaMetadata(typeFixture);
+        });
 
-    afterAll(() => {
-      vitest.clearAllMocks();
-    });
+        afterAll(() => {
+          vitest.clearAllMocks();
+        });
 
-    it('should call getOwnReflectMetadata()', () => {
-      expect(getOwnReflectMetadata).toHaveBeenCalledExactlyOnceWith(
-        typeFixture,
-        schemaOpenApiMetadataReflectKey,
-      );
-    });
+        it('should call getOwnReflectMetadata()', () => {
+          expect(getOwnReflectMetadata).toHaveBeenCalledExactlyOnceWith(
+            typeFixture,
+            schemaOpenApiMetadataReflectKey,
+          );
+        });
 
-    it('should call buildDefaultSchemaMetadata()', () => {
-      expect(buildDefaultSchemaMetadata).toHaveBeenCalledExactlyOnceWith();
-    });
+        it('should call buildDefaultSchemaMetadata()', () => {
+          expect(buildDefaultSchemaMetadata).toHaveBeenCalledExactlyOnceWith();
+        });
 
-    it('should return SchemaMetadata', () => {
-      expect(result).toBe(schemaMetadataFixture);
-    });
-  });
+        it('should return SchemaMetadata', () => {
+          expect(result).toBe(schemaMetadataFixture);
+        });
+      });
 
-  describe('when called, and getOwnReflectMetadata() returns SchemaMetadata', () => {
-    let schemaMetadataFixture: OpenApiSchemaMetadata;
+      describe('when called, and getOwnReflectMetadata() returns SchemaMetadata', () => {
+        let schemaMetadataFixture: OpenApiSchemaMetadata;
 
-    let result: unknown;
+        let result: unknown;
 
-    beforeAll(() => {
-      schemaMetadataFixture = {
-        customAttributes: undefined,
-        name: 'TestSchema',
-        properties: new Map(),
-        references: new Set(),
-        schema: undefined,
-      };
+        beforeAll(() => {
+          schemaMetadataFixture = OpenApiSchemaMetadataFixtures.any;
 
-      vitest
-        .mocked(getOwnReflectMetadata)
-        .mockReturnValueOnce(schemaMetadataFixture);
+          vitest
+            .mocked(getOwnReflectMetadata)
+            .mockReturnValueOnce(schemaMetadataFixture);
 
-      result = getSchemaMetadata(typeFixture);
-    });
+          result = getSchemaMetadata(typeFixture);
+        });
 
-    afterAll(() => {
-      vitest.clearAllMocks();
-    });
+        afterAll(() => {
+          vitest.clearAllMocks();
+        });
 
-    it('should call getOwnReflectMetadata()', () => {
-      expect(getOwnReflectMetadata).toHaveBeenCalledExactlyOnceWith(
-        typeFixture,
-        schemaOpenApiMetadataReflectKey,
-      );
-    });
+        it('should call getOwnReflectMetadata()', () => {
+          expect(getOwnReflectMetadata).toHaveBeenCalledExactlyOnceWith(
+            typeFixture,
+            schemaOpenApiMetadataReflectKey,
+          );
+        });
 
-    it('should not call buildDefaultSchemaMetadata()', () => {
-      expect(buildDefaultSchemaMetadata).not.toHaveBeenCalled();
-    });
+        it('should not call buildDefaultSchemaMetadata()', () => {
+          expect(buildDefaultSchemaMetadata).not.toHaveBeenCalled();
+        });
 
-    it('should return SchemaMetadata', () => {
-      expect(result).toBe(schemaMetadataFixture);
+        it('should return SchemaMetadata', () => {
+          expect(result).toBe(schemaMetadataFixture);
+        });
+      });
     });
   });
 });
