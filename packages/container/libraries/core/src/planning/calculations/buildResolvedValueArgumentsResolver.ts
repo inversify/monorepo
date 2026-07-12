3b8d1fa32fab931fd5261a353122da039b9a3233
notaphplover <roberto.pintos.lopez@gmail.com>
2026-07-12 22:34:31 +0200

    refactor: extract buildResolvedValueArgumentsResolver

---
+++ packages/container/libraries/core/src/planning/calculations/buildResolvedValueArgumentsResolver.ts
@@ -0,0 +1,77 @@
+import { type ResolvedValueBinding } from '../../binding/models/ResolvedValueBinding.js';
+import { type ResolutionParams } from '../../resolution/models/ResolutionParams.js';
+import { type Resolved } from '../../resolution/models/Resolved.js';
+import { type ResolvedValueBindingNode } from '../models/ResolvedValueBindingNode.js';
+import { getGeneratedResolverId } from './getGeneratedResolverId.js';
+
+export function buildResolvedValueArgumentsResolver<TActivated>(
+  node: ResolvedValueBindingNode<ResolvedValueBinding<TActivated>>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  factory: (...args: any[]) => Resolved<TActivated>,
+  resolveActivations: (
+    params: ResolutionParams,
+    resolvedValue: Resolved<TActivated>,
+  ) => Resolved<TActivated>,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  resolveAsyncValues: Function,
+): (params: ResolutionParams) => Resolved<TActivated> {
+  const id: string = getGeneratedResolverId().toString();
+  const argumentIndexes: number[] = Array.from(
+    { length: node.binding.metadata.arguments.length },
+    (_: unknown, index: number) => index,
+  );
+  const resolvedValueConcatenation: string = argumentIndexes
+    .map((index: number) => `value$${index.toString()}`)
+    .join(', ');
+  let resolvedValueDeclarations: string = '';
+
+  for (const index of argumentIndexes) {
+    resolvedValueDeclarations += `const value$${index.toString()} = node$${id}.params[${index.toString()}].resolve(params$${id});\n`;
+  }
+
+  const buildResolveNode: (
+    boundNode: ResolvedValueBindingNode<ResolvedValueBinding<TActivated>>,
+    boundFactory: (...args: unknown[]) => Resolved<TActivated>,
+    activate: (
+      params: ResolutionParams,
+      resolvedValue: Resolved<TActivated>,
+    ) => Resolved<TActivated>,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    resolveAsyncValues: Function,
+    // eslint-disable-next-line @typescript-eslint/no-implied-eval
+  ) => (params: ResolutionParams) => Resolved<TActivated> = new Function(
+    `node$${id}`,
+    `factory$${id}`,
+    `activate$${id}`,
+    `resolveAsyncValues$${id}`,
+    `return function resolveNode$${id}(params$${id}) {
+  ${resolvedValueDeclarations}
+
+  return resolveAsyncValues$${id}(
+    ${resolvedValueConcatenation},
+    function (${resolvedValueConcatenation}) {
+      return activate$${id}(
+        params$${id},
+        factory$${id}(${resolvedValueConcatenation}),
+      );
+    },
+  );
+}`,
+  ) as (
+    node: ResolvedValueBindingNode<ResolvedValueBinding<TActivated>>,
+    factory: (...args: unknown[]) => Resolved<TActivated>,
+    resolveActivations: (
+      params: ResolutionParams,
+      resolvedValue: Resolved<TActivated>,
+    ) => Resolved<TActivated>,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    resolveAsyncValues: Function,
+  ) => (params: ResolutionParams) => Resolved<TActivated>;
+
+  return buildResolveNode(
+    node,
+    factory,
+    resolveActivations,
+    resolveAsyncValues,
+  );
+}
