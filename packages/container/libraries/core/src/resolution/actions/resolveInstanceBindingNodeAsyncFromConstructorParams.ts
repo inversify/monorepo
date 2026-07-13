ad2d09514ba0bbeb253efbce3e5a26d1c8c9330c
notaphplover <roberto.pintos.lopez@gmail.com>
2026-07-14 00:25:11 +0200

    refactor: remove unused function

--- packages/container/libraries/core/src/resolution/actions/resolveInstanceBindingNodeAsyncFromConstructorParams.ts
+++ packages/container/libraries/core/src/resolution/actions/resolveInstanceBindingNodeAsyncFromConstructorParams.ts
@@ -2,23 +2,7 @@ import { type InstanceBinding } from '../../binding/models/InstanceBinding.js';
 import { type InstanceBindingNode } from '../../planning/models/InstanceBindingNode.js';
 import { type ResolutionParams } from '../models/ResolutionParams.js';
 import { type SyncResolved } from '../models/Resolved.js';
-import { resolveBindingServiceActivations } from './resolveBindingServiceActivations.js';
 import { resolveInstanceBindingNodeFromConstructorParams } from './resolveInstanceBindingNodeFromConstructorParams.js';
-
-export async function resolveInstanceBindingNodeAsyncFromOnlyConstructorParams<
-  TActivated,
-  TBinding extends InstanceBinding<TActivated> = InstanceBinding<TActivated>,
->(
-  constructorValues: Promise<unknown[]>,
-  params: ResolutionParams,
-  node: InstanceBindingNode<TActivated, TBinding>,
-): Promise<SyncResolved<TActivated>> {
-  return resolveBindingServiceActivations<TActivated>(
-    params,
-    node.binding.serviceIdentifier,
-    new node.binding.implementationType(...(await constructorValues)),
-  );
-}
 
 export async function resolveInstanceBindingNodeAsyncFromConstructorParams<
   TActivated,
