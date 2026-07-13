4de5df60af4cf73cb4d05c2ab325904ffd1708a8
notaphplover <roberto.pintos.lopez@gmail.com>
2026-07-14 00:09:58 +0200

    feat: add wide instance service in transient scope scenario

--- packages/container/tools/container-benchmarks/src/bin/run.ts
+++ packages/container/tools/container-benchmarks/src/bin/run.ts
@@ -12,35 +12,42 @@ import { AwilixGetComplexServiceInSingletonScope } from '../scenario/awilix/Awil
 import { AwilixGetComplexServiceInTransientScope } from '../scenario/awilix/AwilixGetComplexServiceInTransientScope.js';
 import { AwilixGetServiceInSingletonScope } from '../scenario/awilix/AwilixGetServiceInSingletonScope.js';
 import { AwilixGetServiceInTransientScope } from '../scenario/awilix/AwilixGetServiceInTransientScope.js';
+import { AwilixGetWideServiceInTransientScope } from '../scenario/awilix/AwilixGetWideServiceInTransientScope.js';
 import { Inversify6GetComplexAsyncServiceInTransientScope } from '../scenario/Inversify6/Inversify6GetComplexAsyncServiceInTransientScope.js';
 import { Inversify6GetComplexServiceInSingletonScope } from '../scenario/Inversify6/Inversify6GetComplexServiceInSingletonScope.js';
 import { Inversify6GetComplexServiceInTransientScope } from '../scenario/Inversify6/Inversify6GetComplexServiceInTransientScope.js';
 import { Inversify6GetServiceInSingletonScope } from '../scenario/Inversify6/Inversify6GetServiceInSingletonScope.js';
 import { Inversify6GetServiceInTransientScope } from '../scenario/Inversify6/Inversify6GetServiceInTransientScope.js';
+import { Inversify6GetWideServiceInTransientScope } from '../scenario/Inversify6/Inversify6GetWideServiceInTransientScope.js';
 import { Inversify7GetComplexAsyncServiceInTransientScope } from '../scenario/Inversify7/Inversify7GetComplexAsyncServiceInTransientScope.js';
 import { Inversify7GetComplexServiceInSingletonScope } from '../scenario/Inversify7/Inversify7GetComplexServiceInSingletonScope.js';
 import { Inversify7GetComplexServiceInTransientScope } from '../scenario/Inversify7/Inversify7GetComplexServiceInTransientScope.js';
 import { Inversify7GetServiceInSingletonScope } from '../scenario/Inversify7/Inversify7GetServiceInSingletonScope.js';
 import { Inversify7GetServiceInTransientScope } from '../scenario/Inversify7/Inversify7GetServiceInTransientScope.js';
+import { Inversify7GetWideServiceInTransientScope } from '../scenario/Inversify7/Inversify7GetWideServiceInTransientScope.js';
 import { Inversify8GetComplexAsyncServiceInTransientScope } from '../scenario/inversify8/Inversify8GetComplexAsyncServiceInTransientScope.js';
 import { Inversify8GetComplexServiceInSingletonScope } from '../scenario/inversify8/Inversify8GetComplexServiceInSingletonScope.js';
 import { Inversify8GetComplexServiceInTransientScope } from '../scenario/inversify8/Inversify8GetComplexServiceInTransientScope.js';
 import { Inversify8GetServiceInSingletonScope } from '../scenario/inversify8/Inversify8GetServiceInSingletonScope.js';
 import { Inversify8GetServiceInTransientScope } from '../scenario/inversify8/Inversify8GetServiceInTransientScope.js';
+import { Inversify8GetWideServiceInTransientScope } from '../scenario/inversify8/Inversify8GetWideServiceInTransientScope.js';
 import { InversifyCurrentGetComplexAsyncServiceInTransientScope } from '../scenario/inversifyCurrent/InversifyCurrentGetComplexAsyncServiceInTransientScope.js';
 import { InversifyCurrentGetComplexResolvedValueServiceInTransientScope } from '../scenario/inversifyCurrent/InversifyCurrentGetComplexResolvedValueServiceInTranstientScope.js';
 import { InversifyCurrentGetComplexServiceInSingletonScope } from '../scenario/inversifyCurrent/InversifyCurrentGetComplexServiceInSingletonScope.js';
 import { InversifyCurrentGetComplexServiceInTransientScope } from '../scenario/inversifyCurrent/InversifyCurrentGetComplexServiceInTransientScope.js';
 import { InversifyCurrentGetServiceInSingletonScope } from '../scenario/inversifyCurrent/InversifyCurrentGetServiceInSingletonScope.js';
 import { InversifyCurrentGetServiceInTransientScope } from '../scenario/inversifyCurrent/InversifyCurrentGetServiceInTransientScope.js';
+import { InversifyCurrentGetWideServiceInTransientScope } from '../scenario/inversifyCurrent/InversifyCurrentGetWideServiceInTransientScope.js';
 import { NestCoreGetComplexServiceInSingletonScopeScenario } from '../scenario/nestCore/NestCoreGetComplexServiceInSingletonScopeScenario.js';
 import { NestCoreGetComplexServiceInTransientScopeScenario } from '../scenario/nestCore/NestCoreGetComplexServiceInTransientScopeScenario.js';
 import { NestCoreGetServiceInSingletonScopeScenario } from '../scenario/nestCore/NestCoreGetServiceInSingletonScopeScenario.js';
 import { NestCoreGetServiceInTransientScopeScenario } from '../scenario/nestCore/NestCoreGetServiceInTransientScopeScenario.js';
+import { NestCoreGetWideServiceInTransientScopeScenario } from '../scenario/nestCore/NestCoreGetWideServiceInTransientScopeScenario.js';
 import { TsyringeGetComplexServiceInSingletonScope } from '../scenario/tsyringe/TsyringeGetComplexServiceInSingletonScope.js';
 import { TsyringeGetComplexServiceInTransientScope } from '../scenario/tsyringe/TsyringeGetComplexServiceInTransientScope.js';
 import { TsyringeGetServiceInSingletonScope } from '../scenario/tsyringe/TsyringeGetServiceInSingletonScope.js';
 import { TsyringeGetServiceInTransientScope } from '../scenario/tsyringe/TsyringeGetServiceInTransientScope.js';
+import { TsyringeGetWideServiceInTransientScope } from '../scenario/tsyringe/TsyringeGetWideServiceInTransientScope.js';
 
 export async function run(): Promise<void> {
   // Run get service in singleton scope scenarios
@@ -81,6 +88,29 @@ export async function run(): Promise<void> {
         new AwilixGetServiceInTransientScope(),
         new NestCoreGetServiceInTransientScopeScenario(),
         new TsyringeGetServiceInTransientScope(),
+      ],
+    });
+
+    await benchmark.run();
+
+    printBenchmarkResults(benchmark);
+  }
+
+  // Run get wide service in transient scope scenarios
+  {
+    const benchmark: Bench = buildBenchmark({
+      benchOptions: {
+        name: 'Get wide service in transient scope',
+        time: MS_PER_SCENARIO,
+      },
+      scenarios: [
+        new InversifyCurrentGetWideServiceInTransientScope(),
+        new Inversify6GetWideServiceInTransientScope(),
+        new Inversify7GetWideServiceInTransientScope(),
+        new Inversify8GetWideServiceInTransientScope(),
+        new AwilixGetWideServiceInTransientScope(),
+        new NestCoreGetWideServiceInTransientScopeScenario(),
+        new TsyringeGetWideServiceInTransientScope(),
       ],
     });
 
---
+++ packages/container/tools/container-benchmarks/src/scenario/inversifyCurrent/InversifyCurrentGetWideServiceInTransientScope.ts
@@ -0,0 +1,78 @@
+import { injectable } from '@inversifyjs/core';
+
+import { InversifyCurrentBaseScenario } from './InversifyCurrentBaseScenario.js';
+
+@injectable()
+class Parameter {
+  public value() {
+    return 'value';
+  }
+}
+
+@injectable()
+class WideService {
+  private readonly param1: Parameter;
+  private readonly param2: Parameter;
+  private readonly param3: Parameter;
+  private readonly param4: Parameter;
+  private readonly param5: Parameter;
+  private readonly param6: Parameter;
+  private readonly param7: Parameter;
+  private readonly param8: Parameter;
+  private readonly param9: Parameter;
+  private readonly param10: Parameter;
+
+  constructor(
+    param1: Parameter,
+    param2: Parameter,
+    param3: Parameter,
+    param4: Parameter,
+    param5: Parameter,
+    param6: Parameter,
+    param7: Parameter,
+    param8: Parameter,
+    param9: Parameter,
+    param10: Parameter,
+  ) {
+    this.param1 = param1;
+    this.param2 = param2;
+    this.param3 = param3;
+    this.param4 = param4;
+    this.param5 = param5;
+    this.param6 = param6;
+    this.param7 = param7;
+    this.param8 = param8;
+    this.param9 = param9;
+    this.param10 = param10;
+  }
+
+  public run() {
+    return [
+      this.param1.value(),
+      this.param2.value(),
+      this.param3.value(),
+      this.param4.value(),
+      this.param5.value(),
+      this.param6.value(),
+      this.param7.value(),
+      this.param8.value(),
+      this.param9.value(),
+      this.param10.value(),
+    ].join(' ');
+  }
+}
+
+export class InversifyCurrentGetWideServiceInTransientScope extends InversifyCurrentBaseScenario {
+  public override async setUp(): Promise<void> {
+    this._container.bind(Parameter).toSelf().inTransientScope();
+    this._container.bind(WideService).toSelf().inTransientScope();
+  }
+
+  public async execute(): Promise<void> {
+    this._container.get(WideService);
+  }
+
+  public override async tearDown(): Promise<void> {
+    await this._container.unbindAllAsync();
+  }
+}
