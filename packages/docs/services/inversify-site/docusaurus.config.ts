2ae10db0af1b9fcc43fa3fa93ad2337c1b68e606
notaphplover <roberto.pintos.lopez@gmail.com>
2026-07-20 18:06:03 +0200

    feat: update blog to show 8.2 announcement

--- packages/docs/services/inversify-site/docusaurus.config.ts
+++ packages/docs/services/inversify-site/docusaurus.config.ts
@@ -105,8 +105,8 @@ const config: Config = {
     },
     announcementBar: {
       content:
-        '<a target="_blank" rel="noopener noreferrer" href="/blog/announcing-inversify-8-1-3/">InversifyJS 8.1.3 is out! 🎉️</a>',
-      id: 'announcing_v8_1_3',
+        '<a target="_blank" rel="noopener noreferrer" href="/blog/announcing-inversify-8-2/">InversifyJS 8.2 is out! 🎉️</a>',
+      id: 'announcing_v8_2',
       isCloseable: true,
     },
     footer: {
