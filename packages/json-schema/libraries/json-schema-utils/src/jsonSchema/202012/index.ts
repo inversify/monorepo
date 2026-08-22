bbd22186d9d87407b8f25312c83252e9867f15b6
notaphplover <roberto.pintos.lopez@gmail.com>
2026-08-22 13:54:56 +0200

    feat: expose new modules

--- packages/json-schema/libraries/json-schema-utils/src/jsonSchema/202012/index.ts
+++ packages/json-schema/libraries/json-schema-utils/src/jsonSchema/202012/index.ts
@@ -1,3 +1,13 @@
+export {
+  type DynamicScopeEntry,
+  type LexicalScope,
+  type ResolutionContext,
+  type ResolutionFailure,
+  type SchemaResolutionSuccessLinks,
+  type SchemaResolutionSuccessNode,
+  type SchemaResolutionSuccessTree,
+  JsonSchemaResolver,
+} from './services/JsonSchemaResolver.js';
 export { type TraverseJsonSchemaCallbackParams } from './models/TraverseJsonSchemaCallbackParams.js';
 export { type TraverseJsonSchemaParams } from './models/TraverseJsonSchemaParams.js';
 
